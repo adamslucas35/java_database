4c4c3beec7d8418d2bdb372276d90d6b7c276793
adamslucas35 <adamslucas35@gmail.com>
2023-05-13 19:09:04 +0700

    login gui connects to database and displays errors correctly

--- src/main/java/com/example/c195_aluc167/MainApplication.java
+++ src/main/java/com/example/c195_aluc167/MainApplication.java
@@ -2,7 +2,10 @@ package com.example.c195_aluc167;
 
 import connector.JDBC;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,6 +20,17 @@ public class MainApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void loadScene(String fxmlFile, double width, double height, String title, ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxmlFile));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, width, height);
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
+    }
+
 
 
     public static void main(String[] args) {
--- src/main/java/com/example/c195_aluc167/controllers/LoginMainController.java
+++ src/main/java/com/example/c195_aluc167/controllers/LoginMainController.java
@@ -1,6 +1,7 @@
 package com.example.c195_aluc167.controllers;
 
 import com.example.c195_aluc167.MainApplication;
+import connector.JDBC;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -15,6 +17,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginMainController implements Initializable {
@@ -29,13 +35,65 @@ public class LoginMainController implements Initializable {
         System.out.println("Login page has been initialized!");
     }
 
-    public void lm_login_btn_clicked(ActionEvent actionEvent) throws IOException
-    {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("sample.fxml"));
-        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load(), 927, 366);
-        stage.setScene(scene);
-        stage.setTitle("");
-        stage.show();
+    public void lm_login_btn_clicked(ActionEvent actionEvent) throws IOException, SQLException {
+        if (lm_username_tf.getText().isEmpty() || lm_password_tf.getText().isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Empty fields");
+            alert.setContentText("Please ensure that all text fields are \nfilled with the required information.");
+            alert.showAndWait();
+        }
+        else if (!(lm_username_tf.getText().isEmpty() && lm_password_tf.getText().isEmpty()))
+        {
+
+            PreparedStatement checkCredentials = null;
+            ResultSet resultSet = null;
+            checkCredentials = JDBC.connection.prepareStatement("SELECT Password FROM users WHERE User_Name = ?");
+            checkCredentials.setString(1, lm_username_tf.getText());
+            resultSet = checkCredentials.executeQuery();
+
+            if (!resultSet.isBeforeFirst())
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Provided username is incorrect!");
+                alert.showAndWait();
+            }
+            else
+            {
+                while(resultSet.next())
+                {
+                    String grabPassword = resultSet.getString("Password");
+                    if(grabPassword.equals(lm_password_tf.getText()))
+                    {
+                        MainApplication.loadScene("sample.fxml", 927, 366, "", actionEvent);
+                        // Load sample page
+//                        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("sample.fxml"));
+//                        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+//                        Scene scene = new Scene(fxmlLoader.load(), 927, 366);
+//                        stage.setScene(scene);
+//                        stage.setTitle("");
+//                        stage.show();
+                    }
+                    else
+                    {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("Password is incorrect!");
+                        alert.showAndWait();
+                    }
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
